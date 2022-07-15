5022a734f6e3e788997871f38631c5b2eb9bafa5
Евгений Гилев <euggil@yandex.ru>
2022-07-15 11:49:36 +0500

    Используем ресурсы приложения для цвета сообщения об ошибке

--- HomeApp/HomeApp/Pages/LoginPage.xaml.cs
+++ HomeApp/HomeApp/Pages/LoginPage.xaml.cs
@@ -1,30 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace HomeApp.Pages
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
-        // Константа для текста кнопки
-        public const string BUTTON_TEXT = "Войти";
-        // Переменная счетчика
+        public
+        const string BUTTON_TEXT = "Войти";
         public static int loginCouner = 0;
+
+        // Создаем объект, возвращающий свойства устройства
+        IDeviceDetector detector = DependencyService.Get<IDeviceDetector>();
 
         public LoginPage()
         {
             InitializeComponent();
 
-            // Изменяем внешний вид кнопки для Windows-версии
-            if (Device.RuntimePlatform == Device.UWP)
+            if (Device.Idiom == TargetIdiom.Desktop)
                 loginButton.CornerRadius = 0;
+
+            // Передаем информацию о платформе на экран
+            runningDevice.Text = detector.GetDevice();
         }
 
         /// <summary>
@@ -40,11 +37,11 @@ namespace HomeApp.Pages
             {
                 loginButton.IsEnabled = false;
 
-                // Получаем последний дочерний элемент, используя свойство Children, затем выполняем распаковку
                 var infoMessage = (Label)stackLayout.Children.Last();
-                // Задаем текст элемента
                 infoMessage.Text = "Слишком много попыток! Попробуйте позже";
-
+                // задаем красный цвет сообщения
+                // Используем добавленный глобальный ресурс
+                infoMessage.TextColor = (Color)Application.Current.Resources["errorColor"];
             }
             else
             {
