2591d00f55869ab45dd97d3dd56713182567673e
Евгений Гилев <euggil@yandex.ru>
2022-07-15 09:56:34 +0500

    Добавил фон для IOS и UWP

--- HomeApp/HomeApp.Android/Renderers/CustomEditorRenderer.cs
+++ HomeApp/HomeApp.Android/Renderers/CustomEditorRenderer.cs
@@ -1,6 +1,6 @@
 ﻿using Mobile.Droid.Renderers;
 using Xamarin.Forms;
-
+using Xamarin.Forms.Platform.Android;
 
 [assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
 namespace Mobile.Droid.Renderers
